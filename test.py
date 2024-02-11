2b37e4dfaf1642d7c139802693e93cd353644ce0
dailycoding <dailycoding82@gmail.com>
2024-02-12 04:15:01 +0800

    0_password_manager

---
+++ test.py
@@ -0,0 +1,17 @@
+# coding playground
+"""
+URL: https://www.youtube.com/watch?v=NpmFbWO6HPU
+
+All modified projects done here should 
+be pasted at the bottom of their respected file but commented out. 
+
+Example: 
+modified quiz_game should be pasted at the bottom of 01_quiz_game.py.
+"""
+
+
+"""
+Ideas for improvement:
+- Add guess limits 
+- add game over message if guess limits are reach
+"""
